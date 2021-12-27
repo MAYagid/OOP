97330fee2bfa6bf80f9b9b5434b180d7a5760bab
MAYagid <MAYagid@gmail.com>
2021-12-27 18:47:52 +0300

    HomeWork 8

---
+++ Project/FileMuss/FileMuss/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FileMuss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WindowHeight = 40;
+            Directories current = new()
+            {
+                curdir = Directory.GetCurrentDirectory()
+            };
+            Commands comm = new();
+            string com = "";
+            while (true)
+                
+            {
+                UserInterface.Print(current);
+                comm.Command(current, com);
+            }
+        }
+    }
+}
